950490e72a7b8ea30d3ab76b95843b00a108c7f6
Maksim <maks-vasilev-2017@inbox.ru>
2025-02-10 23:09:41 +0500

    save

--- config/config.go
+++ config/config.go
@@ -1,26 +1,3 @@
-// // config/config.go
-// package config
-
-// // Config хранит настройки приложения
-// type Config struct {
-// 	DBPath       string
-// 	SMTPHost     string
-// 	SMTPPort     int
-// 	SMTPUsername string
-// 	SMTPPassword string
-// }
-
-// // GetConfig возвращает настройки по умолчанию
-// func GetConfig() Config {
-// 	return Config{
-// 		DBPath:       "users.db",            // для SQLite (файл будет создан в корне проекта)
-// 		SMTPHost:     "smtp.gmail.com",      // пример для Gmail
-// 		SMTPPort:     587,
-// 		SMTPUsername: "maks-vasilev-2017@inbox.ru",  // замените на ваш email
-// 		SMTPPassword: "mzgm qflx abxs qqkl",   // замените на ваш пароль или используйте App Password (рекомендуется)
-// 	}
-// }
-
 package config
 
 import (
@@ -61,8 +38,8 @@ func GetConfig() Config {
 
 		SMTPHost:     getEnv("SMTP_HOST", "smtp.gmail.com"),
 		SMTPPort:     smtpPort,
-		SMTPUsername: getEnv("SMTP_USERNAME", "your-email@gmail.com"),
-		SMTPPassword: getEnv("SMTP_PASSWORD", "your-email-password"),
+		SMTPUsername: getEnv("SMTP_USERNAME", "maks-vasilev-2017@inbox.ru"),
+		SMTPPassword: getEnv("SMTP_PASSWORD", "mzgmqflxabxsqqkl"),
 	}
 }
 
