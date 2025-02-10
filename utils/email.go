26ca74d3854704141292dfc52d4a1989000ae44b
Maksim <maks-vasilev-2017@inbox.ru>
2025-02-10 23:54:22 +0500

    save

--- utils/email.go
+++ utils/email.go
@@ -2,11 +2,13 @@ package utils
 
 import (
 	"fmt"
+	"log"
 	"net/smtp"
 )
 
 // SendEmail отправляет письмо через SMTP.
 func SendEmail(smtpHost string, smtpPort int, username, password, to, subject, body string) error {
+	log.Printf("[INFO] Отправка email на %s через %s:%d", to, smtpHost, smtpPort)
 	auth := smtp.PlainAuth("", username, password, smtpHost)
 	from := username
 
@@ -16,7 +18,7 @@ func SendEmail(smtpHost string, smtpPort int, username, password, to, subject, b
 		"To":           to,
 		"Subject":      subject,
 		"MIME-Version": "1.0",
-		"Content-Type": `text/plain; charset="utf-8"`,
+		"Content-Type": "text/plain; charset=utf-8",
 	}
 
 	message := ""
@@ -27,7 +29,10 @@ func SendEmail(smtpHost string, smtpPort int, username, password, to, subject, b
 
 	addr := fmt.Sprintf("%s:%d", smtpHost, smtpPort)
 	if err := smtp.SendMail(addr, auth, from, []string{to}, []byte(message)); err != nil {
+		log.Printf("[ERROR] Ошибка при отправке email: %v", err)
 		return fmt.Errorf("не удалось отправить email: %v", err)
 	}
+
+	log.Println("[INFO] Email успешно отправлен")
 	return nil
 }
